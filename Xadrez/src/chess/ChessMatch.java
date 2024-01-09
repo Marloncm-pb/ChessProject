886f01ce7bb438e9dda829822b7fe6a9d318c9fe
Marlon Carvalho Mariano <marlon.mariano.pb@compasso.com.br>
2024-01-09 14:17:57 -0300

    small adjustments

--- Xadrez/src/chess/ChessMatch.java
+++ Xadrez/src/chess/ChessMatch.java
@@ -10,7 +10,6 @@ import chess.pieces.Bishop;
 import chess.pieces.Knight;
 import chess.pieces.Queen;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -118,7 +117,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
@@ -337,10 +336,6 @@ public class ChessMatch {
         //placing the queens
         placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-
-
-
-
         //placing all the pawns
         for (int i = 0; i < 8; i++) {
             placeNewPiece((char) ('a' + i), 2, new Pawn(board, Color.WHITE, this));
